7840417259e8a61c8aaef40b1b9983ef8ff7caaf
tjgillweb <tjgillweb@gmail.com>
2020-09-03 00:25:18 +0530

    Add Popular Navbar and style it

--- app/index.js
+++ app/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-//Component
-//State
-//Lifecycle
-//UI
+import Popular from './components/Popular'
 
 class App extends React.Component{
     render(){
         return (
-            <div>
-                Hello World!
+            <div className='container'>
+                <Popular />
             </div>
         )
     }
