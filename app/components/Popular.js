e62db951aabdf0d0cdb37c8d5361e83310fa07bd
tjgillweb <tjgillweb@gmail.com>
2020-09-03 20:04:30 +0530

    add selectedLanguage state to track selected menu link

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 export default class Popular extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+    }
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
     render(){
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
         return (
             <ul className='flex-center'>
                 {languages.map((language) => (
                     <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button 
+                            className="btn-clear nav-link"
+                            style={language === this.state.selectedLanguage ? {color: '#9c0c0b'} : null}
+                            onClick={() => this.updateLanguage(language)}>
                             {language}
                         </button>
                     </li>
