2b405c1cf8188b7fe598f20883975fc1fc3f0099
tjgillweb <tjgillweb@gmail.com>
2020-09-10 23:51:34 +0530

    Add Tooltip functionality to ProfileList Component

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,9 +5,57 @@ import Card from './Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
 
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px',
+  }
+}
+
 class ProfileList extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            hoveringLocation: false,
+            hoveringCompany: false
+        }
+
+        this.mouseOver = this.mouseOver.bind(this)
+        this.mouseOut = this.mouseOut.bind(this)
+    }
+
+    mouseOver(id){
+        this.setState({
+            [id]: true
+        })
+    }
+
+    mouseOut(id){
+        this.setState({
+            [id]: false
+        })
+    }
+
     render() {
         const { profile } = this.props
+        const { hoveringLocation, hoveringCompany } = this.state
+        
         return (
             <ul className='card-list'>
                 <li>
@@ -15,13 +63,23 @@ class ProfileList extends React.Component {
                     {profile.name}
                 </li>
                 {profile.location && (
-                <li>
+                <li 
+                    onMouseOver={() => this.mouseOver('hoveringLocation')}
+                    onMouseOut={() => this.mouseOut('hoveringLocation')}
+                    style={styles.container}
+                >
+                    {hoveringLocation === true && <div style={styles.tooltip}>User's Location</div>}
                     <FaCompass color='rgb(144, 115, 255)' size={22} />
                     {profile.location}
                 </li>
                 )}
                 {profile.company && (
-                <li>
+                <li 
+                    onMouseOver={() => this.mouseOver('hoveringCompany')}
+                    onMouseOut={() => this.mouseOut('hoveringCompany')}
+                    style={styles.container}
+                >
+                    {hoveringCompany === true && <div style={styles.tooltip}>User's Company</div>}
                     <FaBriefcase color='#795548' size={22} />
                     {profile.company}
                 </li>
