0c4c029a40e0a1e0001404cd5d77971d72d863e2
tjgillweb <tjgillweb@gmail.com>
2020-09-16 00:18:27 +0530

    Add ThemeProvider using React Context

--- app/index.js
+++ app/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            theme: 'light'
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                    theme: theme === 'light' ? 'dark' : 'light' 
+                }))
+            }
+        }
+    }
     render(){
         return (
-            <div className='container'>
-                {/* <Popular /> */}
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state.theme}>
+                <div className='container'>
+                    {/* <Popular /> */}
+                    <Battle />
+                </div>
+            </ThemeProvider>
         )
     }
 }
