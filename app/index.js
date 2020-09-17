ec7e350fae525a979335b200e12eef2a012bc606
tjgillweb <tjgillweb@gmail.com>
2020-09-17 23:27:21 +0530

    load bundles dynamically as user routes through application using code splitting

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loading from './components/Loading'
+
+// don't import popular until Popular component is going to be rendered i.e. when the path matches index.
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends React.Component{
     state = {
@@ -24,14 +27,16 @@ class App extends React.Component{
                     <div className={this.state.theme}>
                         <div className='container'>
                             <Nav />
-                            {/* Switch: If we have two routes that match only render the first one */}
-                            <Switch> 
-                                <Route exact path='/' component={Popular} />
-                                <Route exact path='/battle' component={Battle} />
-                                <Route path='/battle/results' component={Results} />
-                                <Route render={() => <h1>404</h1>} />
-                                {/* If we leave out the path that Route is always going to render */}
-                            </Switch>
+                            <React.Suspense fallback={<Loading />}>
+                                <Switch> 
+                                    {/* Switch: If we have two routes that match only render the first one */}
+                                    <Route exact path='/' component={Popular} />
+                                    <Route exact path='/battle' component={Battle} />
+                                    <Route path='/battle/results' component={Results} />
+                                    <Route render={() => <h1>404</h1>} />
+                                    {/* If we leave out the path that Route is always going to render */}
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
