0b61a6ae16963ff907ccd957fe75867632217c5d
tjgillweb <tjgillweb@gmail.com>
2020-09-17 19:12:26 +0530

    Add 404

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Battle from './components/Battle'
 import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends React.Component{
     constructor(props){
@@ -28,9 +28,14 @@ class App extends React.Component{
                     <div className={this.state.theme}>
                         <div className='container'>
                             <Nav />
-                            <Route exact path='/' component={Popular} />
-                            <Route exact path='/battle' component={Battle} />
-                            <Route path='/battle/results' component={Results} />
+                            {/* Switch: If we have two routes that match only render the first one */}
+                            <Switch> 
+                                <Route exact path='/' component={Popular} />
+                                <Route exact path='/battle' component={Battle} />
+                                <Route path='/battle/results' component={Results} />
+                                <Route render={() => <h1>404</h1>} />
+                                {/* If we leave out the path that Route is always going to render */}
+                            </Switch>
                         </div>
                     </div>
                 </ThemeProvider>
